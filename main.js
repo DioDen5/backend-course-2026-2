c7a16eae22dc17fc2d3fe23c7309431745f40ff2
Заставний Денис bc2026-2 <denyszastavnyi@gmail.com>
2026-02-23 14:01:35 +0200

    Update main.js with working endpoint

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const { URL } = require('url');
 const date = "20231001";
 const time_period = "q";
 
-const reqUrl = new URL("NBUStatService/v1/statdirectory/banks", "https://bank.gov.ua/");
+const reqUrl = new URL("NBUStatService/v1/statdirectory/exchange", "https://bank.gov.ua/");
 
 reqUrl.searchParams.append("date", date);
 reqUrl.searchParams.append("period", time_period);
